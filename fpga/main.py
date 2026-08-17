6a9ed16bc27b35658653b1ec6240714a533aa277
Matthew Frieri <mat.frieri@gmail.com>
2026-08-17 02:04:09 -0400

    update main

--- fpga/main.py
+++ fpga/main.py
@@ -2,28 +2,51 @@ from fpga.src.bitstream import BitstreamGenerator
 from fpga.src.router import Router
 from fpga.src.node import Node, NodeType
 
-tree = Node(
-    NodeType.OUTPUT,
-    Node(
-        NodeType.OR, 
-        Node(
-            NodeType.NOT, 
-            Node(NodeType.INPUT, io_id=1),
-        ),
-        Node(
-            NodeType.NOT, 
-            Node(
-                NodeType.AND, 
-                Node(NodeType.INPUT, io_id=0),
-                Node(NodeType.INPUT, io_id=2),
-            )
-        ),
-    ),
-    io_id=2
+# tree = Node(
+#     NodeType.OUTPUT,
+#     Node(
+#         NodeType.OR, 
+#         Node(
+#             NodeType.NOT, 
+#             Node(NodeType.INPUT, io_id=1),
+#         ),
+#         Node(
+#             NodeType.NOT, 
+#             Node(
+#                 NodeType.AND, 
+#                 Node(NodeType.INPUT, io_id=0),
+#                 Node(NodeType.INPUT, io_id=2),
+#             )
+#         ),
+#     ),
+#     io_id=2
+# )
+
+shared = Node(
+    NodeType.AND,
+    Node(NodeType.INPUT, io_id=1),
+    Node(NodeType.INPUT, io_id=2),
 )
 
-router = Router(tree, "fpga/fon/fon_3x2_3x3.json")
+Y = Node(
+    NodeType.OUTPUT,
+    Node(
+        NodeType.OR,
+        Node(NodeType.INPUT, io_id=0),
+        shared,
+    ),
+    io_id=0
+)
+Z = Node(
+    NodeType.OUTPUT,
+    shared,
+    io_id=1,
+)
+
+# for lvl, node in Node.level_mapping.items():
+#     print(f"Lvl: {lvl}, {node}")
+router = Router("fpga/fon/fon_3x2_3x3.json")
 routed_fon = router.route()
 
-bitstream = BitstreamGenerator(tree, routed_fon)
+bitstream = BitstreamGenerator(routed_fon)
 bitstream.generate_and_save()
